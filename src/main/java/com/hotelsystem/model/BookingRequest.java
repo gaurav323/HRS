09dbcbfd61ed20e4e04849d313c0ea8eab601256
gaurav323 <62882976+gaurav323@users.noreply.github.com>
2024-01-30 12:00:49 +0530

    Update BookingRequest.java

--- src/main/java/com/hotelsystem/model/BookingRequest.java
+++ src/main/java/com/hotelsystem/model/BookingRequest.java
@@ -1,15 +1,68 @@
-package com.hotelsystem.model;
+package com.hotelbooking.model;
 
 import java.time.LocalDate;
 
 public class BookingRequest {
-	
+
 	private String guestName;
 	private String guestEmail;
 	private String hotelName;
 	private LocalDate checkInDate;
 	private LocalDate checkOutDate;
 	private int noOfGuests;
-	
+
+	public String getGuestName() {
+		return guestName;
+	}
+
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+
+	public String getGuestEmail() {
+		return guestEmail;
+	}
+
+	public void setGuestEmail(String guestEmail) {
+		this.guestEmail = guestEmail;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public LocalDate getCheckInDate() {
+		return checkInDate;
+	}
+
+	public void setCheckInDate(LocalDate checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+
+	public LocalDate getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public void setCheckOutDate(LocalDate checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
+
+	public int getNoOfGuests() {
+		return noOfGuests;
+	}
+
+	public void setNoOfGuests(int noOfGuests) {
+		this.noOfGuests = noOfGuests;
+	}
+
+	public BookingRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
+
