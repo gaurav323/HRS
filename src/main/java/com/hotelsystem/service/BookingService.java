ce67f10419426864de84365fd042bd336e671b8b
gaurav323 <62882976+gaurav323@users.noreply.github.com>
2024-01-30 12:02:04 +0530

    Update BookingService.java

--- src/main/java/com/hotelsystem/service/BookingService.java
+++ src/main/java/com/hotelsystem/service/BookingService.java
@@ -1,5 +1,15 @@
-package com.hotelsystem.service;
+package com.hotelbooking.service;
+
+import com.hotelbooking.model.BookingDetails;
+import com.hotelbooking.model.BookingRequest;
 
 public interface BookingService {
 
+	public String createBooking(BookingRequest bookingRequest);
+
+	public BookingDetails viewBooking(String bookingId);
+
+	public String deleteBooking(String bookingId);
+
 }
+
