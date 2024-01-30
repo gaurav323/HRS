953e08440ed8039905de8f4dd903c753ecd3a6be
gaurav323 <62882976+gaurav323@users.noreply.github.com>
2024-01-30 12:00:21 +0530

    Update BookingDetails.java

--- src/main/java/com/hotelsystem/model/BookingDetails.java
+++ src/main/java/com/hotelsystem/model/BookingDetails.java
@@ -1,5 +1,84 @@
-package com.hotelsystem.model;
+package com.hotelbooking.model;
+
+import java.time.LocalDate;
 
 public class BookingDetails {
 
+	private String bookingId;
+	private String guestName;
+	private String guestEmail;
+	private String hotelName;
+	private LocalDate checkInDate;
+	private LocalDate checkOutDate;
+	private int numberOfGuests;
+	private double totalCost;
+
+	public String getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(String bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public String getGuestName() {
+		return guestName;
+	}
+
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+
+	public String getGuestEmail() {
+		return guestEmail;
+	}
+
+	public void setGuestEmail(String guestEmail) {
+		this.guestEmail = guestEmail;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public LocalDate getCheckInDate() {
+		return checkInDate;
+	}
+
+	public void setCheckInDate(LocalDate checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+
+	public LocalDate getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public void setCheckOutDate(LocalDate checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
+
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+
+	public void setNumberOfGuests(int numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public BookingDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
